93f23e4d0b4d3fa4fbd70f3828db2522acddfa66
Jyrki Pulliainen <jyrki.pulliainen@inoi.fi>
2009-03-30 11:30:48 +0300

    Simplify server start

--- init.d/99server.el
+++ init.d/99server.el
@@ -1,9 +1,1 @@
-(server-start)
 (add-hook 'after-init-hook 'server-start)
-(add-hook 'server-switch-hook
-	  (lambda nil
-	    (let ((server-buf (current-buffer)))
-	      (bury-buffer)
-	      (switch-to-buffer-other-frame server-buf))))
-(add-hook 'server-done-hook 'delete-frame)
-(add-hook 'server-done-hook (lambda nil (kill-buffer nil)))
