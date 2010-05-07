fce8b548f5fbf5bc14bba3f8d48446990dbc53e3
Jyrki Pulliainen <jyrki.pulliainen@inoi.fi>
2010-05-07 15:57:56 +0300

    Change javascript mode to something that actually works

---
+++ init.d/20espresso.el
@@ -0,0 +1,3 @@
+(autoload #'espresso-mode "espresso" "Start espresso-mode" t)
+(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
