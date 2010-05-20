1a69cf435ecabc90c34527ddfdc623bcbd5eafe5
Jyrki Pulliainen <jyrki.pulliainen@inoi.fi>
2010-05-20 13:00:27 +0300

    Add minor changes to various modes

--- init.d/20python.el
+++ init.d/20python.el
@@ -1,4 +1,8 @@
 (require 'python)
 
-(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
-
+(add-hook
+ 'python-mode-hook
+ '(lambda () 
+    (define-key python-mode-map "\C-m" 'newline-and-indent)
+    (c-toggle-auto-hungry-state 1)
+    ))
--- init.d/99cssm.el
+++ init.d/99cssm.el
@@ -5,3 +5,4 @@
 ;; C-style indentations
 (setq cssm-indent-function #'cssm-c-style-indenter)
 (setq cssm-newline-before-closing-bracket t)
+(define-key css-mode-map (kbd "RET") 'newline-and-indent)
