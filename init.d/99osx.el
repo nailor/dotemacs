7dde18522a4ee7090e1eed123a4581b74506ca5b
Jyrki Pulliainen <jyrki@taiste.fi>
2011-01-25 15:49:40 +0200

    Add OS X customizations

---
+++ init.d/99osx.el
@@ -0,0 +1,8 @@
+(setq mac-option-key-is-meta nil)
+(setq mac-comman-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+(setq ring-bell-function (lambda ()))
+(setq mac-allow-anti-aliasing nil)  ;; turn off anti-aliasing
+
+
