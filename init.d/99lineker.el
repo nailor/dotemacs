d679a3337dd2bf908249e0ca6b25ba3e6f79a9de
Jyrki Pulliainen <jyrki@taiste.fi>
2011-02-27 16:41:49 +0200

    Remove useless comment

--- init.d/99lineker.el
+++ init.d/99lineker.el
@@ -1,5 +1,4 @@
 (require 'lineker)
 (add-hook 'c-mode-hook 'lineker-mode)
-;(add-hook 'python-mode-hook 'lineker-mode)
 (add-hook 'log-edit-mode-hook 'lineker-mode)
 (set 'lineker-check-on-save nil)
