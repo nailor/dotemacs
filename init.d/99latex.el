2f3613cc78bcff1ea83b801dce54c8716ce3d98f
Jyrki Pulliainen <jyrki@dywypi.org>
2011-07-24 18:44:06 +0300

    Add ispell and latex

---
+++ init.d/99latex.el
@@ -0,0 +1,8 @@
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+;(setq-default ispell-program-name "tmispell")
+;; (setq ispell-local-dictionary-alist
+;;        (append ispell-local-dictionary-alist
+;;         '(("suomi"
+;;            "[%.0-9A-Za-z\247\300-\326\330-\366\370-\377-]"
+;;            "[^.%0-9A-Za-z\247\300-\326\330-\366\370-\377-]"
+;;            "[':]" nil nil nil utf-8))))
