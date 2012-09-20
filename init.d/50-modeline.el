dcf9f1239ff6a0407f5d2acf3bc912913c6e514e
Jyrki Pulliainen <jyrki@spotify.com>
2012-09-20 13:20:35 +0200

    Use simpler modeline

---
+++ init.d/50-modeline.el
@@ -0,0 +1,21 @@
+;; Set the modeline to tell me the filename, hostname, etc..
+(setq-default mode-line-format
+      (list " "
+            ; */% indicators if the file has been modified
+            'mode-line-modified
+            "--"
+            ; line, column, file %
+            'mode-line-position
+            "--"
+            ; the name of the buffer (i.e. filename)
+            ; note this gets automatically highlighted
+            'mode-line-buffer-identification
+            "--"
+            ; major and minor modes in effect
+            'mode-line-modes
+            ; if which-func-mode is in effect, display which
+            ; function we are currently in.
+            '(which-func-mode ("" which-func-format "--"))
+            "-%-"
+            )
+)
