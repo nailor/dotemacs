ff99a02b8ea702a76bd02d42b366ba98ba51f7b9
Jyrki Pulliainen <jyrki@spotify.com>
2012-09-20 13:20:22 +0200

    better jump-to-mark

---
+++ init.d/30-mark.el
@@ -0,0 +1,6 @@
+(defun jump-to-mark ()
+  "Jumps to the local mark, respecting the `mark-ring' order.
+This is the same as using \\[set-mark-command] with the prefix argument."
+  (interactive)
+  (set-mark-command 1))
+(global-set-key (kbd "M-§") 'jump-to-mark)
