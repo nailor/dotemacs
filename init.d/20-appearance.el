615fe3feb25a859704c9f3d68d4fe278eda53939
Jyrki Pulliainen <jyrki@spotify.com>
2012-09-20 13:18:56 +0200

    Revert theme back to normal

--- init.d/20-appearance.el
+++ init.d/20-appearance.el
@@ -1,2 +1,30 @@
 (if window-system
-    (load-theme 'tomorrow-night)
+  ;; Window system present
+  (progn
+    (set-face-attribute 'default nil
+                        :font "ProggyCleanTT"
+                        :height 120
+                        :background "black"
+                        :foreground "white")
+    (set-face-attribute 'mode-line nil
+                        :background "#f8f8f8"
+                        :foreground "#212121"
+                        :box nil)
+    (set-face-attribute 'mode-line-inactive nil
+                        :background "gray30"
+                        :foreground "gray50"
+                        :box nil)
+    (set-face-attribute 'cursor nil
+                        :background "white"
+                        :foreground "black"))
+
+  ;; Running in console
+  (progn
+    (set-face-attribute 'default nil
+                        :background "black"
+                        :foreground "white")
+    (set-face-attribute 'mode-line nil
+                        :background "white"
+                        :foreground "black"
+                        :inverse-video nil
+                        :box nil)))
