055f47602d6a22189c28ecdf9247ff6309541eb8
Jyrki Pulliainen <jyrki@spotify.com>
2013-04-09 13:56:47 +0200

    Add recent files menu for ido

---
+++ init.d/40-recentf.el
@@ -0,0 +1,4 @@
+(require 'recentf)
+(setq recentf-max-saved-items 200
+      recentf-max-menu-items 15)
+(recentf-mode t)
--- init.d/50-ido-smex.el
+++ init.d/50-ido-smex.el
@@ -21,3 +21,13 @@
 
 (autoload 'idomenu "idomenu" nil t)
 (global-set-key (kbd "M-i") 'idomenu)
+
+;; Add recent files menu
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key (kbd "C-x C-r") 'recentf-ido-find-file)
