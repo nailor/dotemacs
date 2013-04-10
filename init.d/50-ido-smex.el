01b0a8af02e840c092885f4534fd19c3dd864b7f
Jyrki Pulliainen <jyrki@spotify.com>
2013-04-10 09:27:11 +0200

    Abbreviate file names in recent files list

--- init.d/50-ido-smex.el
+++ init.d/50-ido-smex.el
@@ -26,7 +26,10 @@
 (defun recentf-ido-find-file ()
   "Find a recent file using ido."
   (interactive)
-  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+  (let ((file
+         (ido-completing-read
+          "Recent file: "
+          (mapcar 'abbreviate-file-name recentf-list) nil t)))
     (when file
       (find-file file))))
 
