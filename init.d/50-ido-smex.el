a472a20b048366eeb7dc20550d23a26c5c2ac1a2
Jyrki Pulliainen <jyrki@spotify.com>
2013-10-31 13:11:28 +0100

    Display ido results vertically

--- init.d/50-ido-smex.el
+++ init.d/50-ido-smex.el
@@ -18,7 +18,18 @@
 (setq ido-show-dot-for-dired nil)
 (setq ido-auto-merge-delay-time 0.7)
 
+; Vertical ido mode
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
 (smex-initialize)
+
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; This is the old M-x.
