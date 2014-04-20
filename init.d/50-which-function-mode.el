fe78ec0cacd06726404e215da8dfdf24108f1729
Jyrki Pulliainen <jyrki@spotify.com>
2014-04-21 00:51:35 +0200

    Limit which-function to supported modes

--- init.d/50-which-function-mode.el
+++ init.d/50-which-function-mode.el
@@ -5,11 +5,15 @@
 
 (setq which-func-unknown "n/a")
 
-;; Show the current function name in the header line
-(which-function-mode)
-(setq-default header-line-format
-              '((which-func-mode ("" which-func-format " "))))
 (setq mode-line-misc-info
-            ;; We remove Which Function Mode from the mode line, because it's mostly
-            ;; invisible here anyway.
+            ;; We remove Which Function Mode from the mode line,
+            ;; because it's mostly invisible here anyway.
             (assq-delete-all 'which-func-mode mode-line-misc-info))
+
+;; Display which function only on modes that support it
+(add-hook 'after-change-major-mode-hook
+          #'(lambda()
+              (if (member major-mode which-func-modes)
+                  (setq header-line-format
+                        '((which-func-mode ("" which-func-format " "))))
+                (setq header-line-format nil))))
