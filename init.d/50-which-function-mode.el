fad671818bfc23902ae915bce78167c9214a71bd
Jyrki Pulliainen <jyrki@spotify.com>
2014-04-21 00:15:01 +0200

    Add header that shows current method in which-func-mode

--- init.d/50-which-function-mode.el
+++ init.d/50-which-function-mode.el
@@ -2,3 +2,14 @@
 (add-to-list 'which-func-modes 'python-mode)
 (add-to-list 'which-func-modes 'org-mode)
 (which-function-mode 1)
+
+(setq which-func-unknown "n/a")
+
+;; Show the current function name in the header line
+(which-function-mode)
+(setq-default header-line-format
+              '((which-func-mode ("" which-func-format " "))))
+(setq mode-line-misc-info
+            ;; We remove Which Function Mode from the mode line, because it's mostly
+            ;; invisible here anyway.
+            (assq-delete-all 'which-func-mode mode-line-misc-info))
