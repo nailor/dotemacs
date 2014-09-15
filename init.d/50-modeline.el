fa9c53b1cb0d096665f111ece21b0148508979eb
Jyrki Pulliainen <jyrki@spotify.com>
2014-09-15 10:11:05 +0200

    Make modeline niftier

--- init.d/50-modeline.el
+++ init.d/50-modeline.el
@@ -1,18 +1,31 @@
-;; Set the modeline to tell me the filename, hostname, etc..
+(defvar niftier-projectile-mode-line
+  '(:propertize
+    (:eval (when (ignore-errors (projectile-project-root))
+             (concat " " (projectile-project-name))))
+    face font-lock-constant-face)
+  "Mode line format for Projectile.")
+(put 'niftier-projectile-mode-line 'risky-local-variable t)
+
 (setq-default mode-line-format
-      (list " "
-            ; */% indicators if the file has been modified
-            'mode-line-modified
-            "--"
-            ; line, column, file %
-            'mode-line-position
-            "--"
-            ; the name of the buffer (i.e. filename)
-            ; note this gets automatically highlighted
-            'mode-line-buffer-identification
-            "--"
-            ; major and minor modes in effect
-            'mode-line-modes
-            "-%-"
-            )
-)
+              '("%e" mode-line-front-space
+                ;; Standard info about the current buffer
+                mode-line-mule-info
+                mode-line-client
+                mode-line-modified
+                mode-line-remote
+                mode-line-frame-identification
+                mode-line-buffer-identification " " mode-line-position
+                ;; Some specific information about the current buffer:
+                (smartparens-strict-mode (:propertize " ()" face bold))
+                (projectile-mode niftier-projectile-mode-line)
+                (flycheck-mode flycheck-mode-line) ; Flycheck status
+                ;; Misc information, notably battery state and function name
+                " "
+                mode-line-misc-info
+                ;; And the modes, which we don't really care for anyway
+                " " mode-line-modes mode-line-end-spaces)
+              mode-line-remote
+              '(:eval
+                (when-let (host (file-remote-p default-directory 'host))
+                  (propertize (concat "@" host) 'face
+                              '(italic warning)))))
