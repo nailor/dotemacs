cda51e733bb15dc46c7ef754e27322176003b836
Jyrki Pulliainen <jyrki@spotify.com>
2015-07-02 17:04:48 +0200

    Remove some useless conf

--- init.d/50-projectile.el
+++ init.d/50-projectile.el
@@ -1,11 +1,2 @@
 (require 'projectile)
 (projectile-global-mode)
-
-;; Don't run projectile for tramp stuff
-(defadvice projectile-on (around exlude-tramp activate)
-    (unless  (--any? (and it (file-remote-p it))
-        (list
-            (buffer-file-name)
-            list-buffers-directory
-            default-directory))
-    ad-do-it))
