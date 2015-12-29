09d3b570d1f4722d48e971aa2f41cd3e488eb9b2
Jyrki Pulliainen <jyrki@spotify.com>
2015-12-29 10:06:34 +0200

    Diminish things from modeline

--- init.d/99-diminish.el
+++ init.d/99-diminish.el
@@ -1,8 +1,6 @@
 (require 'diminish)
 (diminish 'abbrev-mode)
 (diminish 'yas-minor-mode)
-(diminish 'git-gutter-mode)
-(diminish 'flycheck-mode)
 (diminish 'auto-fill-function)
 (diminish 'projectile-mode)
 (diminish 'auto-complete-mode)
